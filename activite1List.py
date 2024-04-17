b3bbae2cf31be6bf2cd2c62df94ae2452a685212
unknown <imenhzl14@gmail.com>
2024-04-17 12:25:16 +0200

    📦 NEW: activite 1 pour list

---
+++ activite1List.py
@@ -0,0 +1,9 @@
+def chainelist(list):
+    listFinal=[]
+    for elt in list:
+        listFinal.append(elt)
+    return listFinal
+
+list=["A","B","C",1,2,3]
+result=chainelist(list)
+print(result)
